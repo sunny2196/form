f8694d55ce3fa74e8163f9958170a4cb5041514c
sunny2196 <kaliyugiheart@gmail.com>
2025-07-04 21:13:12 +0530

    Add files via upload

---
+++ form.py
@@ -0,0 +1,6 @@
+# Take Name and Email as input from the user
+name = input("Enter your name: ")
+email = input("Enter your email: ")
+
+# Print the received data
+print(f"Received data: Name - {name}, Email - {email}")
